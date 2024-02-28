960833cb066fe52159bc06609327cd8233447fde
Rustam Salimov <rustam2027@yandex.ru>
2024-02-28 17:41:31 +0700

    Add get_iterations method

--- second-task/simple_iterations.py
+++ second-task/simple_iterations.py
@@ -1,6 +1,10 @@
 import math
 
 
+def get_iterations(start_delta, delta, der: float) -> int:
+    return math.ceil(math.log(start_delta/delta)/(-math.log(der)))
+
+
 def simple_iterations(func, initial: float, amount: int) -> float | None:
     previous = initial
     for _ in range(amount):
@@ -9,6 +13,10 @@ def simple_iterations(func, initial: float, amount: int) -> float | None:
     return previous
 
 
+def func(x: float) -> float:
+    return math.tan(x) - x
+
+
 if __name__ == "__main__":
-    # Используется atan так как вблизи нуля у тангенса производная положительная
-    print(simple_iterations(math.atan, 1.25, 100000000))
+    print(get_iterations(2, 0.001, 0.2))
+    print(simple_iterations(func, 0.785, get_iterations(2, 0.001, 0.2)))
