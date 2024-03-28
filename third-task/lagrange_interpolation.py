ed926537d7e87b5d88d82be64fdf074b07560520
Rustam Salimov <rustam2027@yandex.ru>
2024-03-28 22:36:31 +0700

    Correct problems with interpolate points and add graph

--- third-task/lagrange_interpolation.py
+++ third-task/lagrange_interpolation.py
@@ -1,5 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 from typing import List
-from math import cos, pi
+from math import cos, pi, log
 
 
 def omega(roots: List[float], x):
@@ -10,7 +13,7 @@ def omega(roots: List[float], x):
 
 
 def omega_num(roots: List[float], root: float):
-    result = 1
+    result: float = 1
     for root_i in roots:
         if (root == root_i):
             continue
@@ -28,37 +31,42 @@ def get_lagrange(roots: List[float], values: List[float]):
     return f
 
 
-if __name__ == "__main__":
-    n = int(input("Введите число точек интерполяции: "))
-    distrib = int(input(
-        "Введите 1 для равномерного распределение узлов и 2 для распределения по корням Чебышева\n"))
-
-    if distrib == 1:
-        roots = [((2 * i / n) - 1) for i in range(n)]
-    else:
-        roots = [cos(((2 * k + 1) / (2 * n)) * pi) for k in range(n)]
+def get_results(n: int, distrib: int) -> tuple:
+    match distrib:
+        case 1:
+            roots = [((2 * i / n) - (n - 1)/n) for i in range(n)]
+        case 2:
+            roots = [cos(((2 * k + 1) / (2 * n)) * pi) for k in range(n)]
 
     values = [1 / (1 + 25 * root ** 2) for root in roots]
 
     f = get_lagrange(roots, values)
 
-    import matplotlib.pyplot as plt
-    import numpy as np
-
-    fig, ax = plt.subplots()
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
     x = np.linspace(-1, 1, 200)
 
     interpolate_values = [f(i) for i in x]
     function_values = [1 / (1 + 25 * i ** 2) for i in x]
     difference_values = [abs(interpolate_values[i] - function_values[i])
                          for i in range(len(interpolate_values))]
+    return interpolate_values, function_values, difference_values
+
+
+def get_single():
+    n = int(input("Введите число точек интерполяции: "))
+    distrib = int(input(
+        "Введите 1 для равномерного распределение узлов и 2 для распределения по корням Чебышева\n"))
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    x = np.linspace(-1, 1, 200)
+
+    interpolate_values, function_values, difference_values = get_results(
+        n, distrib)
 
     ax.plot(x, interpolate_values, label="Полином Лагранжа")
     ax.plot(x, function_values, label="Функция")
     ax.plot(x, difference_values, label="Разность")
-    ax.scatter(roots, values)
 
     if distrib == 1:
         ax.set_title("Равномерное")
@@ -67,3 +75,29 @@ if __name__ == "__main__":
 
     plt.legend()
     plt.show()
+
+
+def get_graph():
+    max_nodes = int(
+        input("Введите максимальное количество узлов интерполяции: "))
+    step = int(input("Введите шаг между узлами: "))
+    distrib = int(input(
+        "Введите 1 для равномерного распределение узлов и 2 для распределения по корням Чебышева\n"))
+
+    nodes_range = range(2, max_nodes+1, step)
+    max_errors = []
+
+    for nodes in nodes_range:
+        max_errors.append(log(max(get_results(nodes, distrib)[2]), 10))
+
+    plt.plot(nodes_range, max_errors)
+    plt.xlabel('Количество узлов интерполяции')
+    plt.ylabel('Максимальная погрешность')
+    plt.title(
+        'Зависимость максимальной погрешности от количества узлов интерполяции')
+    plt.grid(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    get_graph()
